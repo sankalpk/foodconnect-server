602ff6e88a1f4e1cba6a7c271547d241a117aab9
Sankalp Kulshreshtha <sankalp221@gmail.com>
2016-06-15 22:36:05 -0400

    Use datastore to store forms

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var express = require('express');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var MemcachedStore = require('connect-memcached')(session);
+var gcloud = require('gcloud');
 var bodyParser  = require('body-parser');
+var datastore = gcloud.datastore();
+
 var app = express();
 
 app.use(cookieParser());
@@ -29,9 +32,57 @@ app.get('/', function (req, res, next) {
 });
 
 app.post('/forms/driver', function (req, res) {
-  res.json({
-    data:{
-      message: "Driver form submitted"
+  datastore.save({
+    key: datastore.key('DriverForm'),
+    data: {
+      created: new Date().toJSON(),
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone
+    }
+  }, function (err) {
+    if (err) {
+      res.status(400);
+      res.json({data:{message: "Error. Driver form not submitted"}});
+    } else {
+      res.json({data:{message: "Success. Driver form submitted"}});
+    }
+  });
+});
+
+app.post('/forms/organizer', function (req, res) {
+  datastore.save({
+    key: datastore.key('OrganizerForm'),
+    data: {
+      created: new Date().toJSON(),
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      city: req.body.city
+    }
+  }, function (err) {
+    if (err) {
+      res.status(400);
+      res.json({data:{message: "Error. Organizer form not submitted"}});
+    } else {
+      res.json({data:{message: "Success. Organizer form submitted"}});
+    }
+  });
+});
+
+app.post('/forms/get_updates', function (req, res) {
+  datastore.save({
+    key: datastore.key('GetUpdatesForm'),
+    data: {
+      created: new Date().toJSON(),
+      email: req.body.email,
+    }
+  }, function (err) {
+    if (err) {
+      res.status(400);
+      res.json({data:{message: "Error. Get updates form not submitted"}});
+    } else {
+      res.json({data:{message: "Success. Get updates form submitted"}});
     }
   });
 });
